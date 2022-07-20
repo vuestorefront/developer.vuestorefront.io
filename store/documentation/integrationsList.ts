50e03a4c76cc01bcb6c429bc72dfdbc3c9291753
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-07-19 21:22:53 -0300

    feat: added integrations store

---
+++ store/documentation/integrationsList.ts
@@ -0,0 +1,180 @@
+import { defineStore } from 'pinia';
+import {
+  CommerceIntegrationsName,
+  IntegrationCategory,
+  IntegrationLicense,
+  IntegrationStatus,
+} from '~/enums/integrations';
+import { filterBy } from '~/utils/array';
+import { IntegrationList } from '~/types/integrations';
+
+const isEverythingChecked = (obj: Record<any, any>, key: any) => {
+  if (Object.values(obj[key]).filter(Boolean).length === 0) return '';
+
+  return Object.keys(obj[key]).length ===
+    Object.values(obj[key]).filter(Boolean).length
+    ? 'all'
+    : Object.keys(
+        Object.fromEntries(Object.entries(obj[key]).filter((i) => i[1])),
+      );
+};
+
+export const useIntegrationsList = defineStore('integrationsList', {
+  state: () => {
+    return {
+      filters: {
+        categories: {
+          [IntegrationCategory.analytics]: true,
+          [IntegrationCategory.auth]: true,
+          [IntegrationCategory.cache]: true,
+          [IntegrationCategory.cms]: true,
+          [IntegrationCategory.commerce]: true,
+          [IntegrationCategory.payment]: true,
+          [IntegrationCategory.reviews]: true,
+          [IntegrationCategory.search]: true,
+        },
+        compatibility: {
+          [CommerceIntegrationsName.commercetools]: true,
+          [CommerceIntegrationsName.magento]: true,
+          [CommerceIntegrationsName.sfcc]: true,
+          [CommerceIntegrationsName.sap]: true,
+          [CommerceIntegrationsName.shopify]: true,
+          [CommerceIntegrationsName.bigcommerce]: true,
+          [CommerceIntegrationsName.scayle]: true,
+          [CommerceIntegrationsName.sylius]: true,
+          [CommerceIntegrationsName.woocommerce]: true,
+          [CommerceIntegrationsName.opencart]: true,
+          [CommerceIntegrationsName.vendure]: true,
+          [CommerceIntegrationsName.odoo]: true,
+          [CommerceIntegrationsName.prestashop]: true,
+          [CommerceIntegrationsName.spree]: true,
+          [CommerceIntegrationsName.elasticpath]: true,
+          [CommerceIntegrationsName.swell]: true,
+          [CommerceIntegrationsName.shopware]: true,
+        },
+        status: {
+          [IntegrationStatus.alpha]: true,
+          [IntegrationStatus.beta]: true,
+          [IntegrationStatus.prod]: true,
+          [IntegrationStatus.wip]: true,
+        },
+        license: {
+          [IntegrationLicense.enterprise]: true,
+          [IntegrationLicense.os]: true,
+        },
+      },
+      data: [] as IntegrationList,
+    };
+  },
+  getters: {
+    toggleAllStatus(state) {
+      return Object.fromEntries(
+        Object.keys(state.filters).map((c) => [c, true]),
+      );
+    },
+    filterParams(state) {
+      const baseData = Object.keys(state.filters).reduce((acc, curr) => {
+        if (isEverythingChecked(state.filters, curr)) {
+          return [...acc, [curr, isEverythingChecked(state.filters, curr)]];
+        }
+        return acc;
+      }, []);
+
+      return baseData.length > 0 ? Object.fromEntries(baseData) : {};
+    },
+    integrations(state) {
+      return [
+        {
+          title: 'Commerce Integrations',
+          type: IntegrationCategory.commerce,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.commerce],
+          }),
+        },
+        {
+          title: 'CMS Integrations',
+          type: IntegrationCategory.cms,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, { categories: [IntegrationCategory.cms] }),
+        },
+        {
+          title: 'Payments Integrations',
+          type: IntegrationCategory.payment,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.payment],
+          }),
+        },
+        {
+          title: 'Reviews Integrations',
+          type: IntegrationCategory.reviews,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.reviews],
+          }),
+        },
+        {
+          title: 'Analytics Integrations',
+          type: IntegrationCategory.analytics,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.analytics],
+          }),
+        },
+        {
+          title: 'Auth Integrations',
+          type: IntegrationCategory.auth,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.auth],
+          }),
+        },
+        {
+          title: 'Cache Integrations',
+          type: IntegrationCategory.cache,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.cache],
+          }),
+        },
+        {
+          title: 'Search Integrations',
+          type: IntegrationCategory.search,
+          description:
+            'Posuere vel etiam netus nascetur eget finibus nostra porta rutrum donec facilisi est volutpat eros massa, congue curabitur dapibus gravida ut magna sodales natoque ad neque cursus per primis.',
+          list: filterBy(state.data, {
+            categories: [IntegrationCategory.search],
+          }),
+        },
+      ].filter((i) => Array.isArray(i.list) && i.list.length > 0);
+    },
+    appliedFilters() {
+      return Object.values(this.filterParams).reduce(
+        (acc, curr) => (Array.isArray(curr) ? acc + curr.length : acc),
+        0,
+      );
+    },
+  },
+  actions: {
+    async fetch() {
+      this.data = await $fetch('/api/integrations', {
+        params: this.filterParams,
+      });
+    },
+    toggleAll(key: string) {
+      Object.keys(this.filters[key]).forEach((k) => {
+        this.filters[key][k] = !this.toggleAllStatus[key];
+      });
+
+      this.toggleAllStatus[key] = !this.toggleAllStatus[key];
+    },
+  },
+});
