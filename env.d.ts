352dfbc45d4a4391e8466b2142fa060b916119e8
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-07-26 16:45:54 -0300

    chore: updated project base files

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,24 @@
 /// <reference types="histoire" />
-/// <reference types=".nuxt/nuxt.d.ts" />
+// Nuxt imported added by hand because of WebStorm
+/// <reference types="@nuxt/content" />
+/// <reference types="@intlify/nuxt3" />
+/// <reference types="@pinia/nuxt" />
+/// <reference types="@nuxtjs/tailwindcss" />
+/// <reference types="@vueuse/nuxt" />
+/// <reference types="nuxt-schema-org" />
+/// <reference types="@nuxtjs/algolia" />
+/// <reference types="@nuxt/telemetry" />
+/// <reference types="vue/macros-global" />
+/// <reference types="nuxt" />
+/// <reference path=".nuxt/types/plugins.d.ts" />
+/// <reference path=".nuxt/types/vue-shim.d.ts" />
+/// <reference path=".nuxt/types/schema.d.ts" />
+/// <reference path=".nuxt/types/content.d.ts" />
+/// <reference types="vue-router" />
+/// <reference path=".nuxt/types/middleware.d.ts" />
+/// <reference path=".nuxt/types/layouts.d.ts" />
+/// <reference path=".nuxt/components.d.ts" />
+/// <reference path=".nuxt/types/auto-imports.d.ts" />
+/// <reference path=".nuxt/imports.d.ts" />
+/// <reference path=".nuxt/types/nitro.d.ts" />
 export {};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ import { datetimeFormats } from './locales/dateTimeFormat';
 
 export default defineNuxtConfig({
   head: {
+    titleTemplate: '%s | Vue Storefront - Developer Portal',
     link: [
       {
         rel: 'stylesheet',
@@ -12,6 +13,46 @@ export default defineNuxtConfig({
       },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
     ],
+    meta: [
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Vue Storefront - Developer Portal',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@VueStorefront' },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content:
+          'https://repository-images.githubusercontent.com/497115253/df260e4b-c48c-4725-bf92-54328494a30d',
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content:
+          'https://repository-images.githubusercontent.com/497115253/df260e4b-c48c-4725-bf92-54328494a30d',
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'Nuxt 3',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content:
+          'https://repository-images.githubusercontent.com/497115253/df260e4b-c48c-4725-bf92-54328494a30d',
+      },
+    ],
+  },
+  loading: {
+    color: '#02bb4d',
   },
   debug: process.env.NODE_ENV !== 'production',
   sourcemap: process.env.NODE_ENV !== 'production',
