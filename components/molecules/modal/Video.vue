4cbb75ed278bcc6d3d7530b3a3e55332306e7301
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-08-05 14:14:27 -0300

    refactor: fixed modal size

--- components/molecules/modal/Video.vue
+++ components/molecules/modal/Video.vue
@@ -1,7 +1,12 @@
 <template>
   <AtomsModal :open="open" @close="$emit('close', $event)">
     <template #content>
-      <AtomsVideoPlayer v-if="open" v-bind="$props" />
+      <AtomsVideoPlayer
+        v-if="open"
+        v-bind="$props"
+        :width="`${playerSizes.width}px`"
+        :height="`${playerSizes.height}px`"
+      />
     </template>
   </AtomsModal>
 </template>
@@ -22,4 +27,14 @@
   );
 
   defineEmits(['close']);
+
+  const playerSizes = computed(() => {
+    const { width } = useWindowSize();
+    const widthBase = width.value > 450 ? width.value / 2 : 300;
+
+    return {
+      height: Math.ceil((widthBase * 9) / 16),
+      width: widthBase,
+    };
+  });
 </script>
