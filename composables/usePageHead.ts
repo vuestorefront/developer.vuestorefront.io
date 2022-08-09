f8f34c7762d13c3966be058eddb2a53081dc8994
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-08-09 17:11:16 -0300

    refactor: improved head titles

--- composables/usePageHead.ts
+++ composables/usePageHead.ts
@@ -1,4 +1,5 @@
 import { useI18n } from 'vue-i18n';
+import En from '~/locales/en.json';
 
 export const usePageHead = () => {
   const { t } = useI18n();
@@ -6,7 +7,8 @@ export const usePageHead = () => {
 
   const pageTitle = computed(() => {
     if (route.meta?.title?.startsWith('i18n:')) {
-      return t(`${route.meta.title.split('i18n:').pop()}`);
+      const title = `${route.meta.title.split('i18n:').pop()}`;
+      return t ? t(title) : getValueByKeyString(title, En);
     }
 
     return route.meta.title;
