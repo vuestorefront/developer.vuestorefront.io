eb8cf34662b60e19b1057ea639a13c73ded69dd3
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-09-08 01:36:54 -0300

    refactor: updated packages

--- config/nuxt/algolia.ts
+++
@@ -1,8 +0,0 @@
-import { NuxtConfig } from 'nuxt';
-
-export const nuxtConfigAlgolia = {
-  apiKey: process.env.ALGOLIA_TOKEN,
-  applicationId: process.env.ALGOLIA_ID,
-  lite: false,
-  instantSearch: true,
-} as NuxtConfig['algolia'];
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,85 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable unicorn/relative-url-style */
 import { defineNuxtConfig } from 'nuxt';
-import { nuxtConfigPartyTown } from './config/nuxt/partytown';
-import { nuxtConfigHead } from './config/nuxt/head';
-import { nuxtConfigContent } from './config/nuxt/content';
-import { nuxtConfigI18n } from './config/nuxt/i18n';
-import { nuxtConfigAlgolia } from './config/nuxt/algolia';
-import { nuxtConfigSchema } from './config/nuxt/schema';
-import { nuxtConfigTailwind } from './config/nuxt/tailwind';
-import { nuxtConfigRuntimeConfig } from './config/nuxt/runtimeConfig';
-import { nuxtConfigBuild } from './config/nuxt/build';
-import { nuxtGTagConfig } from './config/nuxt/gtag';
+import { datetimeFormats } from './locales/dateTimeFormat';
 
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ...nuxtConfigBuild,
-  runtimeConfig: nuxtConfigRuntimeConfig,
-  components: true,
+  debug: process.env.NODE_ENV !== 'production',
+  sourcemap: process.env.NODE_ENV !== 'production',
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_API_TOKEN,
+    devToToken: process.env.DEVTO_API_TOKEN,
+    public: {
+      gitHub: {
+        repository: 'vuestorefront/developer.vuestorefront.io',
+        contentPath: 'content',
+        branch: 'main',
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  experimental: {
+    reactivityTransform: true,
+  },
+  build: {
+    transpile: ['@headlessui/vue'],
+  },
   modules: [
     '@intlify/nuxt3',
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
-    ...(process.env.LIGHTHOUSE ? ['@unlighthouse/nuxt'] : []),
     '@vue-storefront/nuxt-gtag',
+    '@nuxt/content',
   ],
-  buildModules: ['@nuxt/content'],
   app: {
-    head: nuxtConfigHead,
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Red+Hat+Text:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap',
+        },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      ],
+    },
   },
   loading: {
     color: '#02bb4d',
   },
-  schemaOrg: nuxtConfigSchema,
-  tailwindcss: nuxtConfigTailwind,
-  content: nuxtConfigContent,
-  intlify: nuxtConfigI18n,
-  algolia: nuxtConfigAlgolia,
-  gTag: nuxtGTagConfig,
+  tailwindcss: {
+    cssPath: '@/assets/scss/main.scss',
+    viewer: false,
+    injectPosition: 0,
+  },
+  content: {
+    highlight: {
+      preload: ['json', 'js', 'ts', 'html', 'css', 'vue'],
+      theme: 'material-darker',
+    },
+  },
+  intlify: {
+    localeDir: 'locales',
+    vueI18n: {
+      datetimeFormats,
+      legacy: false,
+      availableLocales: ['en'],
+      sync: true,
+      fallbackLocale: 'en',
+      locale: 'en',
+    },
+  },
+  gTag: {
+    enabled: process.env.NODE_ENV === 'production',
+    bootstrap: true,
+    pageViewTracker: true,
+    disableScriptLoad: true,
+    config: {
+      id: process.env.GOOGLE_TAG_ID,
+      params: {
+        send_page_view: true,
+      },
+    },
+  },
 });
