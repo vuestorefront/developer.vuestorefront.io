cdaa10dac07d15e58d9c221a9eba73c506ae457a
Sergii Kirianov <s.kirianov@gmail.com>
2023-05-10 12:35:40 +0200

    fix: moved collection of email in the beginning, add consent checkbox (#23)

--- components/active/quiz/UserDetails.vue
+++ components/active/quiz/UserDetails.vue
@@ -3,9 +3,7 @@
     <AtomsTextFirstColoredWord :text="t('page.quiz.userDetails.subheading')" />
   </h1>
 
-  <div
-    class="container mx-auto flex flex-col flex-wrap px-5 py-4 text-gray-600 lg:w-2/3"
-  >
+  <div class="container mx-auto flex flex-col flex-wrap px-5 py-4 text-gray-600 lg:w-2/3">
     <form class="flex flex-col items-center" @submit.stop.prevent="submit">
       <div class="flex flex-col space-y-4 md:w-2/3">
         <!-- Email address -->
@@ -13,13 +11,8 @@
           <label for="email">
             {{ t('page.quiz.userDetails.email') }}
           </label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            required
-            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200"
-          />
+          <input id="email" type="email" name="email" required
+            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200" />
 
           <p class="mt-2 text-center text-sm text-gray-500">
             {{ t('page.quiz.userDetails.emailMessage') }}
@@ -28,12 +21,7 @@
       </div>
 
       <!-- Submit button -->
-      <AtomsButton
-        type="submit"
-        color="primary"
-        class="mt-6"
-        :disabled="loading"
-      >
+      <AtomsButton type="submit" color="primary" class="mt-6" :disabled="loading">
         <AtomsLoading v-if="loading" />
 
         <template v-else>
@@ -45,32 +33,30 @@
 </template>
 
 <script setup lang="ts">
-  import { useI18n } from 'vue-i18n';
-  import type { UserDetails } from '~/types/api/quiz';
+import { useI18n } from 'vue-i18n';
+import type { UserDetails } from '~/types/api/quiz';
 
-  const props = defineProps<{
-    loading: boolean;
-    firstName: string;
-    lastName: string;
-  }>();
+const props = defineProps<{
+  loading: boolean;
+  firstName: string;
+  lastName: string;
+}>();
 
-  const emit = defineEmits<{
-    (e: 'submit', form: UserDetails): void;
-  }>();
+const emit = defineEmits<{
+  (e: 'submit', form: UserDetails): void;
+}>();
 
-  const { t } = useI18n();
+const { t } = useI18n();
 
-  function submit(event: Event) {
-    const form = new FormData(event.target as HTMLFormElement);
+function submit(event: Event) {
+  const form = new FormData(event.target as HTMLFormElement);
 
-    const userDetails = {
-      email: form.get('email') as string,
-      name: props.firstName,
-      surname: props.lastName,
-    };
+  const userDetails = {
+    email: form.get('email') as string,
+    name: props.firstName,
+    surname: props.lastName,
+  };
 
-    console.log(userDetails);
-
-    emit('submit', userDetails);
-  }
+  emit('submit', userDetails);
+}
 </script>
--- pages/quiz/questions/[name].vue
+++ pages/quiz/questions/[name].vue
@@ -23,18 +23,14 @@
           </ul>
           <p>
             Becoming a certified Vue Storefront developer allows you to broaden
-            your career opportunities in the e-commerce space, either by
-            helping you to you to stand out inside your current organisation,
-            or, in case you’re a freelancer, giving you the power to contribute
-            to different projects.
+            your career opportunities in the e-commerce space, either by helping
+            you to you to stand out inside your current organisation, or, in
+            case you’re a freelancer, giving you the power to contribute to
+            different projects.
           </p>
         </div>
         <div class="hidden lg:flex lg:h-96 lg:w-3/5 lg:content-start">
-          <img
-            :src="`/brands/logos/${quiz?.id}.svg`"
-            :alt="quiz?.title"
-            class="h-40 w-full object-contain"
-          />
+          <img :src="`/brands/logos/${quiz?.id}.svg`" :alt="quiz?.title" class="h-40 w-full object-contain" />
         </div>
       </div>
       <div class="w-full lg:w-2/5">
@@ -45,14 +41,8 @@
           <label for="first_name">
             {{ t('page.quiz.userDetails.firstName') }}
           </label>
-          <input
-            id="first_name"
-            v-model="firstName"
-            type="text"
-            name="name"
-            required
-            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200"
-          />
+          <input id="first_name" v-model="firstName" type="text" name="name" required
+            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200" />
         </div>
 
         <!-- Last name -->
@@ -60,155 +50,163 @@
           <label for="last_name">
             {{ t('page.quiz.userDetails.lastName') }}
           </label>
-          <input
-            id="last_name"
-            v-model="lastName"
-            type="text"
-            name="surname"
-            required
-            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200"
-          />
+          <input id="last_name" v-model="lastName" type="text" name="surname" required
+            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200" />
+        </div>
+
+        <!-- Email -->
+        <div class="flex flex-col">
+          <label for="email">
+            {{ t('page.quiz.userDetails.email') }}
+          </label>
+          <input id="email" v-model="email" type="email" name="email" required
+            class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 focus:border-blue-300 focus:ring focus:ring-blue-200" />
         </div>
       </div>
-      <AtomsButton
-        color="primary"
-        :disabled="!firstName || !lastName"
-        label="Start the Quiz"
-        text-color="secondary"
-        class="mt-12 w-full md:w-fit"
-        :shadow="false"
-        @click="startQuiz"
-      />
+
+      <div class="mt-4">
+        <AtomsFormCheckbox :model-value="consent" :value="consent" :section-id="1" @change="onCheckboxChange">
+          {{ t('page.quiz.userDetails.consent') }}
+        </AtomsFormCheckbox>
+      </div>
+      <AtomsButton color="primary" :disabled="!firstName || !lastName || !email || !consent" label="Start the Quiz"
+        text-color="secondary" class="mt-12 w-full md:w-fit" :shadow="false" @click="startQuiz" />
     </div>
 
-    <ActiveQuizSurvey
-      v-if="step === Steps.Survey"
-      :quiz="quiz"
-      :survey-loading="surveyLoading"
-      @submit="submitUserDetails"
-    />
+    <ActiveQuizSurvey v-if="step === Steps.Survey" :quiz="quiz" :survey-loading="surveyLoading"
+      @submit="submitUserDetails" />
   </AtomsLayoutContainer>
 </template>
 
 <script setup lang="ts">
-  import { useI18n } from 'vue-i18n';
-  import { notify } from '@kyvg/vue3-notification';
-  import { ApiUrl } from '~/enums/apiUrl';
-  import type {
-    UserDetails,
-    ApiQuizQuestions,
-    ApiQuizSubmit,
-  } from '~/types/api/quiz';
+import { useI18n } from 'vue-i18n';
+import { notify } from '@kyvg/vue3-notification';
+import { ApiUrl } from '~/enums/apiUrl';
+import type {
+  UserDetails,
+  ApiQuizQuestions,
+  ApiQuizSubmit,
+} from '~/types/api/quiz';
 
-  definePageMeta({
-    documentDriven: false,
-    title: 'i18n:page.quiz.questions.head.title',
-  });
+definePageMeta({
+  documentDriven: false,
+  title: 'i18n:page.quiz.questions.head.title',
+});
 
-  const enum Steps {
-    Intro = 1,
-    Survey = 2,
-    Results = 3,
-  }
+const enum Steps {
+  UserDetails = 0,
+  Intro = 1,
+  Survey = 2,
+  Results = 3,
+}
 
-  const { t } = useI18n();
-  const route = useRoute();
-  const router = useRouter();
-  const config = useRuntimeConfig();
-  const loading = ref(false);
-  const surveyLoading = ref(false);
-  const step = ref(Steps.Intro);
-  const firstName = ref(null);
-  const lastName = ref(null);
-  const form = reactive({
-    selectedAnswers: [] as string[],
-    userDetails: {},
-  });
+const { t } = useI18n();
+const route = useRoute();
+const router = useRouter();
+const config = useRuntimeConfig();
+const loading = ref(false);
+const surveyLoading = ref(false);
+const step = ref(Steps.Intro);
+const firstName = ref(null);
+const lastName = ref(null);
+const email = ref(null);
+const consent = ref(false);
+const form = reactive({
+  selectedAnswers: [] as string[],
+  userDetails: {},
+});
 
-  const { data: quiz } = await useFetch<ApiQuizQuestions>(
-    ApiUrl.QuizQuestions,
-    {
-      key: `quiz-${route.params.name}`,
-      params: {
-        name: route.params.name,
-      },
+console.log(consent.value)
+
+const { data: quiz } = await useFetch<ApiQuizQuestions>(
+  ApiUrl.QuizQuestions,
+  {
+    key: `quiz-${route.params.name}`,
+    params: {
+      name: route.params.name,
     },
+  },
+);
+
+const ogImage = computed(() => {
+  const { href } = new URL(
+    '/og_images/quiz_questions.jpg',
+    config.public.pageUrl,
   );
 
-  const ogImage = computed(() => {
-    const { href } = new URL(
-      '/og_images/quiz_questions.jpg',
-      config.public.pageUrl,
-    );
+  return href;
+});
 
-    return href;
-  });
+useHead({
+  meta: [
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: t('page.quiz.questions.head.ogtitle'),
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: t('page.quiz.questions.head.ogdescription'),
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: ogImage,
+    },
+    {
+      hid: 'og:image:secure_url',
+      property: 'og:image:secure_url',
+      content: ogImage,
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: ogImage,
+    },
+  ],
+});
 
-  console.log(quiz.value.id)
+function startQuiz() {
+  step.value += 1;
+}
 
-  useHead({
-    meta: [
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: t('page.quiz.questions.head.ogtitle'),
-      },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content: t('page.quiz.questions.head.ogdescription'),
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: ogImage,
-      },
-      {
-        hid: 'og:image:secure_url',
-        property: 'og:image:secure_url',
-        content: ogImage,
-      },
-      {
-        hid: 'twitter:image',
-        name: 'twitter:image',
-        content: ogImage,
-      },
-    ],
-  });
+function onCheckboxChange(value: string, event: Event) {
+  const { checked } = event.target as HTMLInputElement;
 
-  function startQuiz() {
-    step.value += 1;
-  }
+  consent.value = !!checked;
+}
 
-  // Methods
-  function submitSurvey(selectedAnswers: string[]) {
-    form.selectedAnswers = selectedAnswers;
-    step.value = Steps.UserDetails;
-  }
+// Methods
+function submitSurvey(selectedAnswers: string[]) {
+  form.selectedAnswers = selectedAnswers;
+  step.value = Steps.UserDetails;
+}
 
-  async function submitUserDetails(selectedAnswers: string[]) {
-    form.selectedAnswers = selectedAnswers;
-    form.userDetails = {
-      name: firstName.value,
-      surname: lastName.value,
-    };
-    surveyLoading.value = true;
-    loading.value = true;
+async function submitUserDetails(selectedAnswers: string[]) {
+  form.selectedAnswers = selectedAnswers;
+  form.userDetails = {
+    name: firstName.value,
+    surname: lastName.value,
+    email: email.value,
+  };
+  surveyLoading.value = true;
+  loading.value = true;
 
-    await $fetch<ApiQuizSubmit>(ApiUrl.QuizSubmit, {
-      method: 'POST',
-      body: {
-        name: quiz.value?.id,
-        ...form,
-      },
-    })
-      .then((data) => router.push(`/quiz/results/${data.id}`))
-      .catch((error) => {
-        loading.value = false;
-        notify({
-          text: error.data.message || t('global.error.generic'),
-          type: 'error',
-        });
+  await $fetch<ApiQuizSubmit>(ApiUrl.QuizSubmit, {
+    method: 'POST',
+    body: {
+      name: quiz.value?.id,
+      ...form,
+    },
+  })
+    .then((data) => router.push(`/quiz/results/${data.id}`))
+    .catch((error) => {
+      loading.value = false;
+      notify({
+        text: error.data.message || t('global.error.generic'),
+        type: 'error',
       });
-  }
+    });
+}
 </script>
